17e08692ae7983904a8e63136a290c83466af7a9
Collier Cobb <collierj.cobb@gmail.com>
2023-08-28 21:15:46 -0400

    organized file structure, created stock users array

---
+++ assets/js/landing-page.js
@@ -0,0 +1,12 @@
+var stockUsers = [
+    {
+        name: "Maria",
+        age: "34",
+        hobbies: ["playing basketball", "programming"],
+    },
+    {
+        name: "Dave",
+        age: "27",
+        hobbies: ["jigsaw puzzles", "reading"],
+    }
+]
