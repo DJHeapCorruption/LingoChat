17de466de6654f3fb712c06aa8123e0ad6f19cec
unknown <r2kayla@gmail.com>
2023-08-30 19:10:59 -0400

    added names and profile information to user array

--- assets/js/users.js
+++ assets/js/users.js
@@ -1,12 +1,26 @@
 var stockUsers = [
     {
-        name: "Maria",
+        name: "John Doe",
         age: "34",
+        targetLanguage: "Spanish",
         hobbies: ["playing basketball", "programming"],
     },
     {
-        name: "Dave",
+        name: "Juan Nadie",
         age: "27",
+        targetLanguage: "Spanish",
         hobbies: ["jigsaw puzzles", "reading"],
+    },
+    {
+        name: "Monsieur X",
+        age: "22",
+        targetLanguage: "Spanish",
+        hobbies: ["reading", "fishing"],
+    },
+    {
+        name: "Max Mustermann",
+        age: "19",
+        targetLanguage: "Spanish",
+        hobbies: ["ice skating", "cooking"],
     }
 ]
