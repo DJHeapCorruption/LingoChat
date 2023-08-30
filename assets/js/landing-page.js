59e60b1a8a097c48819e4a035066e5c8b2384d86
unknown <r2kayla@gmail.com>
2023-08-30 18:48:35 -0400

    moving landing script to user script

--- assets/js/landing-page.js
+++ assets/js/landing-page.js
@@ -1,12 +1,1 @@
-var stockUsers = [
-    {
-        name: "Maria",
-        age: "34",
-        hobbies: ["playing basketball", "programming"],
-    },
-    {
-        name: "Dave",
-        age: "27",
-        hobbies: ["jigsaw puzzles", "reading"],
-    }
-]
+
