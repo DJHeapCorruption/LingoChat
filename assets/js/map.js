046a0e08dea429d49f13f01d006aea7d65c00a35
chuncellor <cbschmidt32@gmail.com>
2023-08-30 20:05:57 -0400

    added map api and basic homepage structure

---
+++ assets/js/map.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
