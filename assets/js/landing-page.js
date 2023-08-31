3dd147fe117e00e560bf5f1818c8339129ef3019
LuminiferousGiraffe <andreas.quist99@gmail.com>
2023-08-30 20:07:09 -0400

    idk merge stuff

--- assets/js/landing-page.js
+++ assets/js/landing-page.js
@@ -1,40 +1,4 @@
 
-
-
-
-var myUser = {
-    firstName: "",
-    lastName: "",
-    knownLang: "",
-    targetLang: ""
-}
-
-function UserCreation (){
-    if (localStorage.getItem("LingoChatUser")){
-        myUser = JSON.parse(localStorage.getItem("LingoChatUser"));
-    } else {
-        $("#exampleModal").modal("show");
-    }
-}
-
-$("#userModalToggleButton").on("click", function(){
-    $("#exampleModal").modal("show");
-});
-
-$("#submit-button").on("click",function(){
-    myUser.firstName = $("#first_name").val();
-    myUser.lastName = $("#last_name").val();
-    myUser.knownLang = $("#known_lang").val();
-    myUser.targetLang = $("#target_lang").val();
-    $("#exampleModal").modal("hide");
-    console.log(myUser);
-});
-
-UserCreation();
-
-
-
-
 var myUser = {
     firstName: "",
     lastName: "",
