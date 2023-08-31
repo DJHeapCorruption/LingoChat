e349df0e368705a285f90eb6b79f708e94c124f0
LuminiferousGiraffe <andreas.quist99@gmail.com>
2023-08-30 20:03:01 -0400

    Added HTML and JavaScript for user creation modal

--- assets/js/landing-page.js
+++ assets/js/landing-page.js
@@ -10,3 +10,35 @@ var stockUsers = [
         hobbies: ["jigsaw puzzles", "reading"],
     }
 ]
+
+
+
+var myUser = {
+    firstName: "",
+    lastName: "",
+    knownLang: "",
+    targetLang: ""
+}
+
+function UserCreation (){
+    if (localStorage.getItem("LingoChatUser")){
+        myUser = JSON.parse(localStorage.getItem("LingoChatUser"));
+    } else {
+        $("#exampleModal").modal("show");
+    }
+}
+
+$("#userModalToggleButton").on("click", function(){
+    $("#exampleModal").modal("show");
+});
+
+$("#submit-button").on("click",function(){
+    myUser.firstName = $("#first_name").val();
+    myUser.lastName = $("#last_name").val();
+    myUser.knownLang = $("#known_lang").val();
+    myUser.targetLang = $("#target_lang").val();
+    $("#exampleModal").modal("hide");
+    console.log(myUser);
+});
+
+UserCreation();
