ab03e4f69cd00a1a41b97c4790aa297b58281c4a
unknown <r2kayla@gmail.com>
2023-08-31 21:38:03 -0400

    get location feature in map

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,11 +1,9 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map');
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
-
-L.marker([51.5, -0.09]).addTo(map);
-
+map.locate ({setView: true});
 
 
